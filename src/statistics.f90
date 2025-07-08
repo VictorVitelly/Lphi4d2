2293e6fc6a318c7c414c27563aa8e58433c9e00f
Victor Vitelly <victor-vitelly@ciencias.unam.mx>
2025-07-08 15:07:54 -0600

    Implemented Swendsen-Wang multi cluster subroutine.

--- src/statistics.f90
+++ src/statistics.f90
@@ -82,7 +82,7 @@ contains
       end do
     end do
   end subroutine metropolis
-
+  
   subroutine flip_sign(phi,i)
     real(dp), dimension(N,N), intent(inout) :: phi
     integer(i4), intent(in) :: i
@@ -95,6 +95,105 @@ contains
       end do
     end if
   end subroutine flip_sign
+  
+  subroutine cluster(m0,phi) 
+    real(dp),intent(in) :: m0
+    real(dp), dimension(N,N),intent(inout) :: phi
+    integer(i4), dimension(N,N) :: spin
+    logical, dimension(N,N) :: bond_x,bond_y
+    integer(i4) :: i,j,label(N,N),parent(N*N),next_label,left_label,up_label
+    logical, allocatable :: flip_cluster(:)
+    real(dp) :: beta,r,p
+    
+    spin(:,:)=nint(sign(1._dp,phi(:,:)),i4)
+    
+    do i=1,N
+      do j=1,N
+        if(spin(i,j)==spin(mod(i,N)+1,j) ) then
+          beta=abs(phi(i,j))*abs(phi(mod(i,N)+1,j))
+          p=1._dp-exp(-2._dp*beta )
+          call random_number(r)
+          bond_x(i,j)=(r<p)
+        else
+          bond_x(i,j)=.false.
+        end if
+        if(spin(i,j)==spin(i,mod(j,N)+1) ) then
+          beta=abs(phi(i,j))*abs(phi(i,mod(j,N)+1))
+          p=1._dp-exp(-2._dp*beta )
+          call random_number(r)
+          bond_y(i,j)=(r<p)
+        else
+          bond_y(i,j)=.false.
+        end if
+      end do
+    end do
+
+    label(:,:)=0
+    do i=1,N*N
+      parent(i)=i
+    end do
+    next_label=1
+    left_label=0
+    up_label=0
+
+    do i=1,N
+      do j=1,N
+        left_label=0
+        up_label=0
+        if(i>1 .and. bond_x(i-1,j) ) then
+          left_label=label(i-1,j)
+        end if
+        if(j>1 .and. bond_y(i,j-1) ) then
+          up_label=label(i,j-1)
+        end if
+        if(left_label==0 .and. up_label==0) then
+          label(i,j)=next_label
+          next_label=next_label+1  
+        else if(left_label /= 0 .and. up_label==0) then
+          label(i,j)=left_label
+        else if(left_label== 0 .and. up_label/=0) then
+          label(i,j)=up_label
+        else
+          label(i,j)=min(left_label,up_label)
+          call union(left_label,up_label,parent)
+        end if
+      end do
+    end do
+    
+    do j=1,N
+      if(bond_x(N,j) ) then
+        call union(label(1,j),label(N,j),parent )
+      end if
+    end do
+    do i=1,N
+      if(bond_y(i,N) ) then
+        call union(label(i,1),label(i,N),parent )
+      end if 
+    end do
+    do i=1,N
+      do j=1,N
+        label(i,j)=find(label(i,j),parent)
+      end do
+    end do
+
+    allocate(flip_cluster(next_label) )
+    flip_cluster(:)=.false.
+    do i=1,next_label-1
+      call random_number(r)
+      flip_cluster(i)=(r<0.5_dp)
+    end do
+    
+    do i=1,N
+      do j=1,N
+        if(flip_cluster(label(i,j))) then
+          phi(i,j)=-phi(i,j)
+        end if
+      end do
+    end do
+    deallocate(flip_cluster)
+    
+  end subroutine cluster
+
 
   !Statistics for measurements
   !
